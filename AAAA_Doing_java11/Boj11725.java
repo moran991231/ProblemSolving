974671b602daa87cb21c933ba85c07b49081e3c9
moran991231 <moran991231@naver.com>
2021-05-21 18:17:07 +0900

    molco

--- AAAA_Doing_java11/Boj11725.java
+++ AAAA_Doing_java11/Boj11725.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
+class Boj11725 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
