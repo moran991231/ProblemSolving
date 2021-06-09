2849d436f09d754a25bd438bb4d3721487f7a9a2
jspark-noticekorea <moran991231@naver.com>
2021-06-09 10:37:29 +0900

    last bfs problem

--- AAAA_Doing_java11/Boj2573.java
+++ AAAA_Doing_java11/Boj2573.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
+class Boj2573 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
